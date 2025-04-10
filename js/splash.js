4c9261494fe7c5db8074e1999c5a0dc2f9775969
juanse68 <163223814+juanse68@users.noreply.github.com>
2025-04-10 01:58:17 +0000

    17

--- js/splash.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-      document.getElementById("splash").classList.add("oculto");
-      document.getElementById("main").classList.remove("oculto");
-    }, 2000);
-  });
-  
