0fb2c2197ce6c725e5bc7c095fe162d200c79ae9
juanse68 <163223814+juanse68@users.noreply.github.com>
2025-04-10 01:24:42 +0000

    7

---
+++ js/home.js
@@ -0,0 +1,14 @@
+import { obtenerFestivos } from './api.js';
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const contenedor = document.getElementById("home");
+  contenedor.innerHTML = "<h2>Festivos en Austria 2025</h2>";
+
+  const festivos = await obtenerFestivos();
+  festivos.forEach(f => {
+    const div = document.createElement("div");
+    div.className = "festivo";
+    div.innerHTML = `<strong>${f.localName}</strong> - ${f.date}`;
+    contenedor.appendChild(div);
+  });
+});
