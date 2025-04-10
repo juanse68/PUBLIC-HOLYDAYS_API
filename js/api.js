2a08398a63d582eeca61bbdbd06bcc6ab0e90851
duban77 <159207588+duban77@users.noreply.github.com>
2025-04-10 15:50:51 +0000

    19

--- js/api.js
+++ js/api.js
@@ -1,8 +1,10 @@
-fetch('https://date.nager.at/api/v3/PublicHolidays/2025/ES')
-  .then(response => response.json())
-  .then(data => {
-    console.log('Feriados:', data);
-  })
-  .catch(error => {
-    console.error('Error al obtener los feriados:', error);
-  });
+export async function obtenerFestivos() {
+  try {
+    const respuesta = await fetch('https://date.nager.at/api/v3/PublicHolidays/2025/AT'); // AT = Austria
+    const datos = await respuesta.json();
+    return datos;
+  } catch (error) {
+    console.error("Error al obtener los festivos:", error);
+    return [];
+  }
+}
